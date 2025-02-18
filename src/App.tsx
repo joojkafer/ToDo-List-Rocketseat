c84a7371281637c642c4c511ce11573a456dd3b7
joojkafer <joaoantoniokaferribeiro@gmail.com>
2025-02-18 17:48:13 -0300

    textbox, logo and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import './App.css'
+import styles from './App.module.css'
+import Logo from './assets/Logo.svg'
+//import { Rocket } from "@phosphor-icons/react";
 
 export function App() {
+    return (
+      <div>
+        <header className={styles.header}>
+            <img src={Logo}/>
+        </header>
+        
+        <div className={styles.wrapper}>
+          {/* Textbox */}
+          <div className={styles.textBox}>
+            <textarea
+              required
+              placeholder='Adicione uma nova tarefa'
+            />
+          </div>
 
-  return (
-    <div>
-      teste
-    </div>
-  )
+          {/* Create button */}
+          <div className={styles.createButton}>
+            <button className={styles.btn}>
+              Criar
+            </button>
+          </div>
+        </div>
+
+      </div>
+    )
 }
